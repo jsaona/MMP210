5e8797d03c3d4a6835a405ed8115d79f49ae01c2
jsaona <jsaona1995@hotmail.com>
2017-09-05 20:50:35 -0400

    initial commit

---
+++ self-portrait/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+	createCanvas(640, 460);
+}
+function draw() {
+	background("white");
+	fill("yellow")
+	
+    ellipse(200, 200, 200, 200);
+    
+    fill("black");
+    ellipse(160, 170, 30, 30);
+    
+    ellipse(235, 170, 30, 30);
+    
+    ellipse(195, 245, 75, 20);
+}

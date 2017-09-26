296726e1337c6a32c3c075cb700f94994a9a3bd4
jsaona <jsaona1995@hotmail.com>
2017-09-26 15:16:04 -0400

    assignment4

---
+++ assignment4/sketch.js
@@ -0,0 +1,12 @@
+function setup() { 
+  createCanvas(400, 400);
+	 background(220);
+     stroke(400) 
+     
+} 
+function draw() {
+  background(0);
+
+    
+	line(mouseY, mouseX, pmouseX, pmouseX);
+}
